a29454dcdc3ce592ddd0106453042bdcd42f9ee6
MariaesSegura <msegber@gmail.com>
2020-06-30 09:07:41 +0200

    Get random number

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+let result = getRandomNumber(100);
+document.addEventListener('DOMContentLoaded', getRandomNumber);
+console.log(result);
